006bf5862d458ba2b700cc134c4d9aa91cb8563a
Rasulbek Musayev <rasulbeksoftwareengineer@gmail.com>
2025-02-04 14:08:44 +0500

    Xatolar bilan ishlash

--- src/pages/dashboard-home.tsx
+++ src/pages/dashboard-home.tsx
@@ -4,21 +4,40 @@ import { useAppContext } from "@/contexts/app.context";
 import { ContentCreateRequestParam } from "@/shared/types/content-create-request-params";
 import { generateArticle } from "@/utils/openai";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function DashboardHome() {
-    const {setGeneratingContent, generatingContent} = useAppContext();
+    
+    const { setGeneratingContent, generatingContent } = useAppContext();
+    
     const [content, setContent] = useState<string | null>(null);
+    
     const handleSubmit = async (params: ContentCreateRequestParam) => {
         setGeneratingContent(true);
-        const {title, descrption} = params;
-        const result = await generateArticle(title, descrption);
-        setContent(result)
-        setGeneratingContent(false);
+        const { title, descrption } = params;
+        
+        try {
+        
+            const result = await generateArticle(title, descrption);
+        
+            setContent(result);
+        }
+        
+        catch (error) {
+        
+            console.error('[Error] Failed to generate article', error);
+            toast.error("Afsuski AI bilan bo'gliq muommolar mavjud.")
+        }
+
+        finally{
+            setGeneratingContent(false);
+        }
     }
+    
     return (
         <div>
             <h1 className="text-3xl font-semibold">Article Writer</h1>
-            {content ? <ContentViewer content={content}/> : <ContentCreateForm isLoading={generatingContent} onSubmit={handleSubmit}/>}
+            {content ? <ContentViewer content={content} /> : <ContentCreateForm isLoading={generatingContent} onSubmit={handleSubmit} />}
         </div>
     )
 };
